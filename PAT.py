5a8a71c3adcdf3a613fe623bb4b73ed4da7901a1
tharvik <tharvik@gmail.com>
2012-06-30 20:33:58 +0000

    Translation of the messages

--- PAT.py
+++ PAT.py
@@ -17,17 +17,14 @@ import urllib   # Download Google's results
 import sys      # To flush!
 
 
-## Functions
-
-
 ## Script
 
 # Parse arguments
-parser = argparse.ArgumentParser (description="PAT: Plagiat Auto-Tester; essaye de determiner combien du travail entré se trouve sur Google.")
-parser.add_argument ("file", type=file, help="Texte à tester")
+parser = argparse.ArgumentParser (description="PAT: Plagiat Auto-Tester; try to determine how much of your text is on Google.")
+parser.add_argument ("file", type=file, help="Text to test")
 args = parser.parse_args()
 
-print "Chargement du texte...",
+print "Loading text...",
 
 # Open text file, get contents
 with args.file as textFile :
@@ -37,8 +34,8 @@ with args.file as textFile :
 del args
 del parser
 
-print "fait!"
-print "Traitement du texte:",
+print "done!"
+print "Text processing:",
 
 # Making a nice list
 sentences = re.split ("[ -'\n]", text)
@@ -73,33 +70,33 @@ for i in range (totalLine - howManyWordsByCheck) :
             sentence = text
 
     print
-    print "  Traitement du bloc de phrases [" + str (i+1) + "/" + str (totalLine - howManyWordsByCheck) + "]:"
-    print "   Téléchargement des résultats Google...",
+    print "  Processing of text-bloc [" + str (i+1) + "/" + str (totalLine - howManyWordsByCheck) + "]:"
+    print "   Downloading Google's result...",
     
     # Everyday in flusheling
     sys.stdout.flush()
     
-    (fileName, Null) = urllib.urlretrieve ("http://www.google.fr/search?q=\"" + urllib.quote_plus (sentence) + "\"")
+    (fileName, Null) = urllib.urlretrieve ("http://www.google.co.uk/search?q=\"" + urllib.quote_plus (sentence) + "\"")
     
-    print "fait!"
-    print "   Analyse de la page...",
+    print "done!"
+    print "   Page processing...",
         
     plagiat = False
     with open (fileName) as page :
 
-        if not re.search ("www.gstatic.com/m/images/icons/warning.gif|Aucun document ne correspond", page.read ()) :
+        if not re.search ("www.gstatic.com/m/images/icons/warning.gif| - did not match any documents.", page.read ()) :
             plagiat = True
             
     
-    print "fait!"
+    print "done!"
     
     if plagiat :
-        print "    /!\\ Oops, plagiat detecté dans \"" + sentence + "\""
+        print "    /!\\ Oops, plagiat detected in \"" + sentence + "\""
         totalPlagiat = totalPlagiat + 1
     
     
     
 print "--"
-print str (totalPlagiat) + " occurences de plagiat on été trouvées"
-print str (totalLine - howManyWordsByCheck)    + " mots ont été traitées"
-print "Ce qui nous donne un magnifique total de " + str (totalPlagiat * 100 / (totalLine - howManyWordsByCheck)) + "% de plagiat! (valeur indicative)"
+print str (totalPlagiat) + " plagiat's occurence were found"
+print str (totalLine - howManyWordsByCheck)    + " words were processed"
+print "what give us a beautiful score of " + str (totalPlagiat * 100 / (totalLine - howManyWordsByCheck)) + "% of palgiat! (more or less)"
